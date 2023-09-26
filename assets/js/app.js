ff18bfeb2b107b1ea9e413d1959d16605fe454c3
Ethan Chaney <echaney456@gmail.com>
2023-09-25 20:08:47 -0500

    added functionality to start and solve buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,8 @@ import { drawBoard } from "./draw_board.js";
 
 /* Global Variable */
 let selectedNum = null;
+let grid = Sudoku.generateGrid();
+let sol = Sudoku.solvePuzzle();
 
 /* initializing the board */
 drawBoard();
@@ -26,5 +28,19 @@ drawBoard();
 })
 
 //TODO: add functionality to button to initialize the board
+document.querySelector(".start-btn").addEventListener("click", function() {
+    for(let r=0; r<9; r++) {
+        for(let c=0; c<9; c++) {
+            document.getElementById(`${r}-${c}`).innerText = grid[r][c];
+        }
+    }
+});
 
 //TODO: add functionality to solve button
+document.querySelector(".solve-btn").addEventListener("click", function() {
+    for(let r=0; r<9; r++) {
+        for(let c=0; c<9; c++) {
+            document.getElementById(`${r}-${c}`).innerText = sol[r][c];
+        }
+    }
+});
